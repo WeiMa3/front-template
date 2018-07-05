893e29add226e05771942f9a7a4f36f7c73eb705
WeiMa3 <vagrant@scotchbox>
2018-07-05 19:35:39 +0000

    Update image and file

--- test.php
+++ test.php
@@ -1,1 +1,171 @@
-testtest
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:600|Raleway:700" rel="stylesheet">
+    <title>MyTest</title>
+
+    <style type="text/css">
+
+    	.headlogo{
+    		width:80px;
+    	}
+
+    	.nav-link{
+    		font-family: 'Raleway', sans-serif;
+    		font-size:13px;
+    		color:#666;
+    		letter-spacing: 3px
+    	}
+
+    	.p-info{
+    		font-family:'Montserrat',sans-serif;
+    		font-size:14px;
+    		color:#666;
+    		letter-spacing: 1px
+    	}
+    	.cardLogo{
+    		width:80px;
+    	}
+    	.cardSubtitle{
+    		font-family:'Montserrat',sans-serif;
+    		font-size:13px;
+    		color:#666;
+    		letter-spacing: 1px
+    	}
+    	.cardContent{
+    		font-family:'Montserrat',sans-serif;
+    		font-size:11px;
+    		color:#666;
+    		letter-spacing: 1px
+    	}
+    	.cardImg{
+    		width:33vw;
+    		height:25vw;
+    	}
+
+    	.footer{
+    		height:500px;
+    		background-color:#444;
+    	}
+    </style>
+
+
+
+
+  </head>
+  <body>
+
+    <div class="container fontMain sticky-header">
+<!-- logo -->
+    	<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530506578636&di=38f46b4e6af57ae69199166a80bb3d32&imgtype=0&src=http%3A%2F%2Fimg14.huitu.com%2Fres%2F20140722%2F580742_20140722172708973146_2.jpg" class="headlogo my-2 mx-auto d-block">	
+<!-- nav bar -->
+    	<ul class="nav nav-pills nav-fill">
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Home</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Link 1</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Link 2</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Link 3</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Link 4</a>
+		  </li>
+		</ul>
+    </div>
+<!-- video -->
+    <div class="container-fluid px-0 my-4">
+    	<iframe style="width:100vw; height:56vw" src="https://www.youtube.com/embed/hs86eBCT4dI?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+<!-- cards -->
+    <div class="container">
+    	<div class="text-center my-5 p-info">
+    		<p>They serve the purpose of changing hydrogen into breathable oxygen, <br>and they’re as necessary here as the air is, on Earth.</p>
+    	</div>
+    	<div class="row mx-auto text-center">
+    		<div class="col-4">
+    			<div>
+    				<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530506578636&di=38f46b4e6af57ae69199166a80bb3d32&imgtype=0&src=http%3A%2F%2Fimg14.huitu.com%2Fres%2F20140722%2F580742_20140722172708973146_2.jpg" class="cardLogo">
+    			</div>
+    			<div class="cardSubtitle my-3">
+    				<p>Subtitle 1</p>
+    			</div>
+    			<div class="cardContent">
+    				<p>"Do you sell them?"</p>
+    			</div>
+    		</div>
+    		<div class="col-4">
+    			<div>
+    				<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530506578636&di=38f46b4e6af57ae69199166a80bb3d32&imgtype=0&src=http%3A%2F%2Fimg14.huitu.com%2Fres%2F20140722%2F580742_20140722172708973146_2.jpg" class="cardLogo">
+    			</div>
+    			<div class="cardSubtitle my-3">
+    				<p>Subtitle 2</p>
+    			</div>
+    			<div class="cardContent">
+    				<p>"I'm afraid, not."</p>
+    			</div>
+    		</div>
+    		<div class="col-4">
+    			<div>
+    				<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530506578636&di=38f46b4e6af57ae69199166a80bb3d32&imgtype=0&src=http%3A%2F%2Fimg14.huitu.com%2Fres%2F20140722%2F580742_20140722172708973146_2.jpg" class="cardLogo">
+    			</div>
+    			<div class="cardSubtitle my-3">
+    				<p>Subtitle 3</p>
+    			</div>
+    			<div class="cardContent">
+    				<p>"But... maybe we can make a deal."</p>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+<!-- cards for pictures -->
+    <div class="container-fluid my-5">
+    	<div class="row ">
+    		<div class="col-4">
+    			<img src="http://charismaticplanet.com/wp-content/uploads/2015/06/Forget-Me-Nots-flowers-25785400-900-719.jpg" class="cardImg">
+    		</div>
+    		<div class="col-4">
+    			<img src="https://i.pinimg.com/736x/fa/1a/2b/fa1a2b970bc6ef6c656fb3fe44326392--dahlia-flowers-colorful-flowers.jpg" class="cardImg">
+    		</div>
+    		<div class="col-4">
+    			<img src="https://coolcreativity.com/wp-content/uploads/2015/12/How-to-Change-The-Color-of-Hydrangea-Flowers.jpg" class="cardImg">
+    		</div>
+    		<div class="col-4">
+    			<img src="https://jixifox.files.wordpress.com/2017/05/cherry-blossoms-flowers-1.jpg" class="cardImg">
+    		</div>
+    		<div class="col-4">
+    			<img src="http://animals.sandiegozoo.org/sites/default/files/2017-12/animals_hero_plumeria.jpg" class="cardImg">
+    		</div>
+    		<div class="col-4">
+    			<img src="https://image.jimcdn.com/app/cms/image/transf/none/path/s004d1ab5c61c9a43/image/i2e29fc083bbb029c/version/1490458299/image.jpg" class="cardImg">
+    		</div>
+    		<div class="container text-center p-info mt-5">
+    			<a href="https://www.google.ca/search?rlz=1C1SQJL_zh-CNCA797CA797&biw=1452&bih=634&tbm=isch&sa=1&ei=yYE6W5qBGKfn5gLtsIzADg&q=flowers&oq=flowers&gs_l=img.12...0.0.0.977.0.0.0.0.0.0.0.0..0.0....0...1c..64.img..0.0.0....0.Cr2kWDbakL0">MORE...</a>
+    		</div>
+    	</div>
+    </div>
+
+<!-- footer -->
+	<div class="container-fluid footer">
+		
+	</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  </body>
+</html>
